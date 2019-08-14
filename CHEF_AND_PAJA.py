7d2b3795f9d599fc00d46d98113d42a35713bedc
MrHackIT <rakesh127001@gmail.com>
2019-08-14 15:08:15 +0530

    CHEF and PAJA

---
+++ CHEF_AND_PAJA.py
@@ -0,0 +1,16 @@
+print("value of t")
+t = int(input())
+
+i = int
+for i in range(t):
+    x = int(input())
+    y = int(input())
+    k = int(input())
+    t = (x+y)/k
+    if t%2 ==0:
+        print("chef")
+    else:
+        print("paja")
+
+
+
